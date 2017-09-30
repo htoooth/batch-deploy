9a653a70e4b653c9b1f9b7711770c5a1fb994054
htoooth <ht.anglenx@gmail.com>
2017-09-30 14:18:06 +0800

    fix

--- fabfile.py
+++ fabfile.py
@@ -1,2 +1,42 @@
-def hello():
-  print("hello world!")
+from fabric.api import *
+
+env.user = 'root'
+env.password = '123456'
+env.hosts = [
+  '192.168.104.37',
+  '192.168.104.50',
+  '192.168.104.65',
+  '192.168.104.73',
+  '192.168.104.77',
+  '192.168.104.78',
+  '192.168.104.82',
+]
+
+pc_dir = '/home/test/yohobuy-node'
+h5_dir = '/home/test/yohobuywap-node'
+
+src_dir = pc_dir
+
+@task
+def name():
+  run('hostname')
+
+@task
+def pull():
+  with cd(src_dir):
+    run('git pull')
+
+@task
+def install():
+  with cd(src_dir):
+    run('npm i')
+
+@task
+def deploy():
+  with cd(src_dir):
+    run('npm run prod')
+
+@task
+def branch():
+  with cd(src_dir):
+    run('git branch')
